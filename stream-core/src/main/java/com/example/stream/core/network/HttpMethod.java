05b6a42a77d5c0df1f36e4197fd6958285d2e96a
streamj <mystream@vip.qq.com>
2017-08-13 01:57:59 +0800

    add retrofits

---
+++ stream-core/src/main/java/com/example/stream/core/network/HttpMethod.java
@@ -0,0 +1,15 @@
+package com.example.stream.core.network;
+
+/**
+ * Created by StReaM on 8/13/2017.
+ */
+
+public enum  HttpMethod {
+    GET,
+    POST,
+    POST_RAW,
+    PUT,
+    PUT_RAW,
+    DELETE,
+    UPLOAD
+}
---
+++ stream-core/src/main/java/com/example/stream/core/network/RestClient.java
@@ -0,0 +1,8 @@
+package com.example.stream.core.network;
+
+/**
+ * Created by StReaM on 8/13/2017.
+ */
+
+public class RestClient {
+}
